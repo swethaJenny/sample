2e7ac2948a3909042e0a8595fada27d24dbd6ce7
swethaJenny <swethamca2002@gmail.com>
2024-02-24 16:05:35 +0530

    demo java file is created

---
+++ demo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class demo{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello world");
+        int num = scan.nextInt();
+        System.out.println(num);
+        scan.close();
+    }
+}
