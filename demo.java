2db4c129d567390ca898f2ed449ae428e0cca09c
swethaJenny <swethamca2002@gmail.com>
2024-02-24 16:09:31 +0530

    demo java file is modified

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class demo{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello world");
+        System.out.println("Hello swetha");
         int num = scan.nextInt();
         System.out.println(num);
         scan.close();
